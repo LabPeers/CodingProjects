f72b30134b0bfbfa03d7a2f5cc63e0b95eb26f5c
LabPeers <62810593+LabPeers@users.noreply.github.com>
2020-11-03 14:59:57 -0600

    Add files via upload

---
+++ DataEngineer/E2_crawling/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='UrPartsCrawler',
+    version='0.1dev',
+    packages=['crawler',],
+    #license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
